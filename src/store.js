84a9589b8da1c9f7db0bb7359e882fd56a9233ef
Austin Papritz <austin@papritz.dev>
2023-09-15 11:06:00 -0700

    added headshot18.jpeg

--- src/store.js
+++ src/store.js
@@ -20,8 +20,7 @@ const state = {
       header: "Diamond Road",
       image: "/photo-1519608487953-e999c86e7455.jpeg",
       aspect: 1.5,
-      text:
-        "The man who comes back through the Door in the Wall will never be quite the same as the man who went out. He will be wiser but less sure, happier but less self-satisfied, humbler in acknowledging his ignorance yet better equipped to understand the relationship of words to things, of systematic reasoning to the unfathomable mystery which it tries, forever vainly, to comprehend."
+      text: "The man who comes back through the Door in the Wall will never be quite the same as the man who went out. He will be wiser but less sure, happier but less self-satisfied, humbler in acknowledging his ignorance yet better equipped to understand the relationship of words to things, of systematic reasoning to the unfathomable mystery which it tries, forever vainly, to comprehend."
     },
     {
       offset: 3,
@@ -29,8 +28,7 @@ const state = {
       header: "Catalina",
       image: "/ph1.jpg",
       aspect: 1.5037,
-      text:
-        "The substance can take you to heaven but it can also take you to hell. Or else to both, together or alternately. Or else (if you're lucky, or if you've made yourself ready) beyond either of them. And then beyond the beyond, back to where you started from — back to here, back to New Rotham sted, back to business as usual. Only now, of course, business as usual is completely different."
+      text: "The substance can take you to heaven but it can also take you to hell. Or else to both, together or alternately. Or else (if you're lucky, or if you've made yourself ready) beyond either of them. And then beyond the beyond, back to where you started from — back to here, back to New Rotham sted, back to business as usual. Only now, of course, business as usual is completely different."
     },
     {
       offset: 4,
@@ -38,8 +36,7 @@ const state = {
       header: "Building 21",
       image: "/ph3.jpg",
       aspect: 0.665,
-      text:
-        "We’ve found that the people whose EEG doesn’t show any alpha-wave activity when they’re relaxed aren’t likely to respond significantly to the substance. That means that, for about fifteen percent of the population, we have to find other approaches to liberation."
+      text: "We’ve found that the people whose EEG doesn’t show any alpha-wave activity when they’re relaxed aren’t likely to respond significantly to the substance. That means that, for about fifteen percent of the population, we have to find other approaches to liberation."
     },
     {
       offset: 5,
@@ -47,8 +44,7 @@ const state = {
       header: "Sector 8",
       image: "/photo-1533577116850-9cc66cad8a9b.jpeg",
       aspect: 1.55,
-      text:
-        "By cultivating the state of mind that makes it possible for the dazzling ecstatic insights to become permanent and habitual illuminations. By getting to know oneself to the point where one won’t be compelled by one’s unconscious to do all the ugly, absurd, self-stultifying things that one so often finds oneself doing."
+      text: "By cultivating the state of mind that makes it possible for the dazzling ecstatic insights to become permanent and habitual illuminations. By getting to know oneself to the point where one won’t be compelled by one’s unconscious to do all the ugly, absurd, self-stultifying things that one so often finds oneself doing."
     },
     { offset: 7, factor: 1.05, header: "The Factory", image: "/photo-1548191265-cc70d3d45ba1.jpeg", aspect: 1.77, text: "Education and enlightenment." }
   ],
