166ffb3152c103b63142789d6b1ff175cab77046
Austin Papritz <austin@papritz.dev>
2023-09-29 13:57:06 -0700

    added Items component

---
+++ src/components/Items.jsx
@@ -0,0 +1,19 @@
+import Item from "./Item.jsx"
+import { proxy, useSnapshot } from "valtio"
+
+const state = proxy({
+	clicked: null,
+	urls: [1, 2, 3].map((u) => `/${u}.jpg`)
+})
+
+function Items({ w = 0.7, gap = 0.15 }) {
+	const { urls } = useSnapshot(state)
+	const xW = w + gap
+	return (
+		<group position={[5, 0, 0]}>
+			{urls.map((url, i) => (
+				<Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={url} />
+			))}
+		</group>
+	)
+}
